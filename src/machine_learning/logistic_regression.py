1b480fc3095952f1fdba6de78b8cdec239caf1ff
Grup7-Proje <grup7proje@proton.me>
2026-05-31 13:57:53 +0300

    Update logistic_regression.py

--- src/machine_learning/logistic_regression.py
+++ src/machine_learning/logistic_regression.py
@@ -3,12 +3,19 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
+import joblib # EKSİK 1: Modeli ve Scaler'ı kaydetmek için gerekli
 
 X_train = pd.read_csv('X_train_hazir.csv')
 X_test  = pd.read_csv('X_test_hazir.csv')
 y_train = pd.read_csv('y_train_hazir.csv').squeeze()
 y_test  = pd.read_csv('y_test_hazir.csv').squeeze()
 
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+joblib.dump(scaler, 'scaler_lr.pkl') # Scaler dosyası analizcilere gidiyor
+print("Veri ölçeklendirildi ve 'scaler_lr.pkl' kaydedildi.")
 
 # MODELİ İSKELETİ. !!!WEİGHT BALANCİNG!!!
 # class_weight='balanced'  kanser olanlar az olduğu için azınlık sınıfına daha fazla ağırlık vermeli ki model hatalı olmasın.
@@ -53,9 +60,19 @@ y_pred  = best_lr.predict(X_test_scaled)
 y_prob  = best_lr.predict_proba(X_test_scaled)[:, 1]
 
 # SONUÇLARI KAYDETTİK. BU YENİ DOSYALAR ANALİZ UZMANLARI TARAFINDAN KULLANILACAK.
+joblib.dump(best_lr, 'kanser_taramasi_lr_model.pkl')
+print("Model 'kanser_taramasi_lr_model.pkl' olarak kaydedildi.")
 
 np.save('y_pred_lr.npy', y_pred)
 np.save('y_prob_lr.npy', y_prob)
 
 print("Tahminler kaydedildi: y_pred_lr.npy, y_prob_lr.npy")
 print("Lojistik Regresyon basaraili.")
+
+katsayilar = pd.DataFrame({
+    'Öznitelik': X_train.columns,
+    'Katsayı (Etki)': best_lr.coef_[0]
+}).sort_values(by='Katsayı (Etki)', ascending=False)
+
+katsayilar.to_csv('oznitelik_katsayilari_lr.csv', index=False)
+print("Öznitelik katsayıları 'oznitelik_katsayilari_lr.csv' olarak kaydedildi.")
